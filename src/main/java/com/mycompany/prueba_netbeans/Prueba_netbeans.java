ceb0abec7a40ced1aa68e36e1ba155da306212c1
AlumnoMañana <AlumnoMaana@DESKTOP-KK9UC82>
2024-02-12 10:11:48 +0100

    Este código imprime Hello World! en ingles

---
+++ src/main/java/com/mycompany/prueba_netbeans/Prueba_netbeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba_netbeans;
+
+/**
+ *
+ * @author AlumnoMañana
+ */
+public class Prueba_netbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
